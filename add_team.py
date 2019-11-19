06a495dcde48ebda62d782dbb9a528593e03a418
igorgvero <46729371+igorgvero@users.noreply.github.com>
2019-11-19 13:48:01 -0800

    Update add_team.py

--- add_team.py
+++ add_team.py
@@ -3,12 +3,8 @@ import json
 import requests
 from veracode_api_signing.plugin_requests import RequestsAuthPluginVeracodeHMAC
 
-# Workspace - 87ede3a7-941d-4442-beee-2e122de58f1d
-# team- 84132
-
 api_base = "https://api.veracode.com/srcclr/v3"
 headers = {"User-Agent": "Create Workspace Example"}
-
 
 if __name__ == "__main__":
 
