cf2bc5a0858a879f40e4d75057eedd570ee878e4
igorgvero <46729371+igorgvero@users.noreply.github.com>
2019-11-19 15:25:32 -0600

    Add files via upload

---
+++ get_applications.py
@@ -0,0 +1,24 @@
+import sys
+import requests
+from veracode_api_signing.plugin_requests import RequestsAuthPluginVeracodeHMAC
+
+
+api_base = "https://api.veracode.com/appsec/v1"
+headers = {"User-Agent": "Get Applications Example"}
+
+
+if __name__ == "__main__":
+
+    try:
+        response = requests.get(api_base + "/applications", auth=RequestsAuthPluginVeracodeHMAC(), headers=headers)
+    except requests.RequestException as e:
+        print("Whoops!")
+        print(e)
+        sys.exit(1)
+
+    if response.ok:
+        data = response.json()
+        for app in data["_embedded"]["applications"]:
+            print(app["profile"]["name"] + " --- " + app["guid"])
+    else:
+        print(response.status_code)
